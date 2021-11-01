d431379d3a30c860a5342672e10ff23e340fd84a
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-10-31 21:21:37 -0800

    5 vectors created to use with the struct.

--- vector-stats/vector-stats.cpp
+++ vector-stats/vector-stats.cpp
@@ -5,6 +5,7 @@
 //
 
 #include <iostream>
+#include <vector>
 
 struct vectorStats {
     int min;
@@ -15,6 +16,13 @@ struct vectorStats {
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector <int> s1{ 1, 26,7, 14,5,2 };
+    std::vector <int> s2{ 5,10,15,20, 25 };
+    std::vector <int> s3{ 16, 0, 14, 43, 78, 19, 200 };
+    std::vector <int> s4{ 2, 10, 14, 17, 0, 22, 8 };
+    std::vector <int> s5{ 0,2,17,0,15, 29, 8 };
+
+
+
 }
 
